c68cd3592844a752a99f692d8effa273ab8d4556
drone-xb81 <giovannicode7@gmail.com>
2015-02-04 18:36:55 -0600

    added vagrant support

---
+++ users/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, first_name, last_name, email, password):
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password):
+        user=self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    first_name = models.CharField()
+    last_name = models.CharField
+    email = models.CharField()
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = email
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __unicode__(self):
+        return self.email
