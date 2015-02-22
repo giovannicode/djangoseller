43695c7d07946c6dccf370bc12628e0f01df85a9
drone-xb81 <giovannicode7@gmail.com>
2015-02-22 16:44:14 -0600

    update seller

--- products/static/products/js/ctrl.js
+++ products/static/products/js/ctrl.js
@@ -1,33 +1,58 @@
 angular.module('productsApp', [])
   .controller('productsController', function($scope, $http, $location){
-    angular.element(document).ready(function(){
-      $scope.getproducts(0);
-    })
+    /*/angular.element(document).ready(function(){
+      $scope.getproducts($location.path());
+    })*/
     $scope.data = {};
-    $scope.getproducts = function(category_id){
-      url = "/products/api/list?format=json";
-      if(!category_id)
+    $scope.alert = function(mssg){
+      alert(mssg);
+    };
+    $scope.$watch(
+      function(){
+        return $location.path(); 
+      }, 
+      function(newValue, oldValue){
+        if(newValue == '/products/list')
+        {
+          url = newValue.replace('products/list', 'products/api/list?format=json');
+        }
+        else
+        {
+          url = newValue.replace('products/list?', 'products/api/list?format=json&');
+        }
+        $scope.getproducts(url);
+      }
+    );
+
+    $scope.update_url = function(category_id){
+      url = 'products/list';
+      url += "?categories=" + category_id;
+      $location.path(url);
+    };
+
+    $scope.update_filterurl = function(type, value){
+      url = $location.path();
+      var regex = new RegExp(type + '=-?[a-z]+');
+      if(url.search(type) != -1)
       {
+        url = url.replace(regex, type + "=" + value);
       }
       else
       {
-        get_param = "categories=" + category_id;
-        url += "&" + get_param;
+        url += "&" + type + "=" + value;
       }
+      $scope.$apply(function(){
+        $location.path(url);
+      });
+    }
+      
+    $scope.getproducts = function(url){
+     
       var responsePromise = $http.get(url);
 
 
       responsePromise.success(function(data, status, headers, config){
         $scope.data = data;
-        client_url = "/products/list";
-        if(!category_id)
-        {
-          $location.path(client_url);
-        } 
-        else
-        {
-          $location.path(client_url + '?' + get_param);
-        }
       });
       responsePromise.error(function(data, status, headers, config){
         alert("Ajax failed!"); 
