10ac809bda250c0f4b48e5f1ce0c84db55bdb141
drone-xb81 <giovannicode7@gmail.com>
2015-02-26 15:38:36 -0600

    deleted user_form.html from account templates

--- account/views.py
+++ account/views.py
@@ -1,1 +1,10 @@
+from django.views.generic import TemplateView
 
+class AccountView(TemplateView)
+    template_name = 'account/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountView, self).get_context_data(**kwargs)
+        user = self.request.user
+        context['orders'] = user.order_set.all()
+        return context
