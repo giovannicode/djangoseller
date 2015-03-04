93e82a614940cbc956ef8b0b1bc9ab61d1520b77
drone-xb81 <giovannicode7@gmail.com>
2015-03-04 11:15:38 -0600

    add unique=true contraint to session_key property on Cart model

--- carts/models.py
+++ carts/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from products.models import Product
 
 class Cart(models.Model):
-    session_key = models.CharField(max_length=40, null=True)
+    session_key = models.CharField(max_length=40, unique=True, null=True)
 
     def __unicode__(self):
         return str(self.id)
