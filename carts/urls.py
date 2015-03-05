a97ef7572e4cf4ee2c939c5ccb3a7ebd863c4645
drone-xb81 <giovannicode7@gmail.com>
2015-03-05 13:18:24 -0600

    remove pk arguments from CartDetailView

--- carts/urls.py
+++ carts/urls.py
@@ -4,5 +4,5 @@ import views
 
 urlpatterns = patterns('',
     url(r'^add', views.CartCreateRest.as_view(), name='add'),
-    url(r'^(?P<pk>[0-9]+)$', views.CartDetailView.as_view(), name='detail')
+    url(r'^$', views.CartDetailView.as_view(), name='detail')
 )
