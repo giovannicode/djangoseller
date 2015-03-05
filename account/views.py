791019800137566afe03d13582a7f921eb703a4a
drone-xb81 <giovannicode7@gmail.com>
2015-03-05 12:51:42 -0600

    add Q objects in orders filter to get all orders associated with the user and their corresponding email address

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import TemplateView
 
 from orders.models import Order
@@ -8,6 +9,5 @@ class AccountView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(AccountView, self).get_context_data(**kwargs)
         user = self.request.user
-        context['orders'] = user.order_set.all()
-        context['orders'] += Order.objects.filter(email=user.email, user=none).
+        context['orders'] = Order.objects.filter(Q(user=user) | Q(user=None, email=user.email))
         return context
