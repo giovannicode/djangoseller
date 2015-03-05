ffb7b1c2a8c3532b4049013f3cd6c7a05ea6545c
drone-xb81 <giovannicode7@gmail.com>
2015-03-05 13:00:26 -0600

    add LoginRequiredMixin to AccountView

--- account/views.py
+++ account/views.py
@@ -1,10 +1,13 @@
 from django.db.models import Q
 from django.views.generic import TemplateView
 
+from braces.views import LoginRequiredMixin
+
 from orders.models import Order
 
-class AccountView(TemplateView):
+class AccountView(LoginRequiredMixin, TemplateView):
     template_name = 'account/index.html'
+    login_url = '/users/signin'
 
     def get_context_data(self, **kwargs):
         context = super(AccountView, self).get_context_data(**kwargs)
