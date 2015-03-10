7dc7da89a4e91af260e229fd4974861b21a3194d
drone-xb81 <giovannicode7@gmail.com>
2015-03-10 14:03:43 -0500

    changed name of Filter attribute. From tag to name

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from categories.models import Category
 
 class Filter(models.Model):
-    tag = models.CharField(max_length='30')
+    name = models.CharField(max_length='30')
 
     def __unicode__(self):
         return self.tag  
