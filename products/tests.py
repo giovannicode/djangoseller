6ff1234c0169b2fc9bab8e41fda9ce075a47d0b5
drone-xb81 <giovannicode7@gmail.com>
2015-03-12 17:55:31 -0500

    Create ProductTests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductTests(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name='gio shirt', description='cool shirt', picture=)    
+
+    def test_create_product(self):
+        product = Products.objects.get(name='cup')
+        self.assertEqual(product.name, 'cup')
