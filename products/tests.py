35c8cc4090f8c3980c16ec864d662c918499372d
drone-xb81 <giovannicode7@gmail.com>
2015-03-13 13:23:04 -0500

    delete tests

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,7 @@
+from django.db import IntegrityError
 from django.test import TestCase
 from .models import Product
 
-class ProductTests(TestCase):
 
-    def test_create_product(self):
-        self.asserRaises(IntegrityError, Product, name='seven')
+class ProductTests(TestCase):
+    pass
