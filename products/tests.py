72f29dfe97fe29de848710fd158d50699b45a55a
drone-xb81 <giovannicode7@gmail.com>
2015-03-13 10:28:16 -0500

    edit test to check for exception. If exception is thrown, the test should pass

--- products/tests.py
+++ products/tests.py
@@ -2,9 +2,6 @@ from django.test import TestCase
 from .models import Product
 
 class ProductTests(TestCase):
-    def setUp(self):
-        self.product = Product.objects.create(name='gio shirt', description='cool shirt', picture=)    
 
     def test_create_product(self):
-        product = Products.objects.get(name='cup')
-        self.assertEqual(product.name, 'cup')
+        self.asserRaises(IntegrityError, Product, name='seven')
