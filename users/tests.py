0a5f98640ab82abb2179693c7a2773de748d98dd
drone-xb81 <giovannicode7@gmail.com>
2015-03-13 15:51:56 -0500

    add method to test the UserCreateForm

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from users.models import UserCreateForm
 
-# Create your tests here.
+class UserTests(TestCase):
+     def test_UserCreateForm_invalid_inputs(self): 
+         form_data = {
+             'username': 'crazycat@giovannicode.com', 
+             'password': 'password5',
+         }
+         form = UserCreateForm(data=form_data)         
+         self.assertEqual(form.is_invalid(), True) 
