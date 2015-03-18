6702de87c8c641b2da33fa1399f2e7b615346426
drone-xb81 <giovannicode7@gmail.com>
2015-03-18 11:57:31 -0500

    change user to root in supervisor config file and add supvervisorctl to playbook

--- ansible/config_supervisor.sh
+++ ansible/config_supervisor.sh
@@ -1,8 +1,8 @@
 cat << EOF > /etc/supervisor/conf.d/gunicorn.conf
 [program:gunicorn]
-command=gunicorn -c /home/vagrant/www/gunicorn/conf.py seller.wsgi
-directory=/home/vagrant/www/website
-user=vagrant
+command=gunicorn -c /home/root/www/gunicorn/conf.py seller.wsgi
+directory=/home/root/www/website
+user=root
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/supervisor/gunicorn/err.log
