f7a2ab46d9fecf5bec4c5b12f6276fcf3d7c2c9d
drone-xb81 <giovannicode7@gmail.com>
2015-03-19 13:31:36 -0500

    add prepare_server method

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,13 @@ from fabric.api import local
 #def test():
 #    local("python manage.py test")
 
+def prepare_server():
+    local("ansible-playbook -u root ansible/playbook.yml")
+
 def push():
     local("git push production master")
 
 def deploy():
 #    test()
+    prepare_server():
     push()
