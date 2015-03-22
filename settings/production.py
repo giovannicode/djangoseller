3553955430ff8d0363856ac63b914915dba081f4
drone-xb81 <giovannicode7@gmail.com>
2015-03-22 18:07:48 -0500

    Add email support in settings/production.py

--- settings/production.py
+++ settings/production.py
@@ -8,3 +8,10 @@ DATABASES = {
         'PORT': '',
         }
     }
+
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = ['EMAIL_HOST_PASSWORD']
