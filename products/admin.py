aee89ff3438d65ad921b429578407fe4207273f1
drone-xb81 <giovannicode7@gmail.com>
2015-03-23 15:41:39 -0500

    Make ProductAdmin 'select box' widgets 20 rows long, instead of using the default

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.forms import SelectMultiple
+from django.db import models
 
 from .models import Product, Tag 
 
+class ProductAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': SelectMultiple(attrs={'size':'20'})}, 
+    }
+
 admin.site.register(Tag)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
