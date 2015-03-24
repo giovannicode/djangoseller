a6a21372037dc9933020b6020cbfdccb0cbb436b
drone-xb81 <giovannicode7@gmail.com>
2015-03-24 16:06:47 -0500

    Add TagAdmin and set ordering

--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,15 @@ from django.forms import SelectMultiple
 from django.db import models
 
 from .models import Product, Tag 
+from categories.models import Category
 
 class ProductAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.ManyToManyField: {'widget': SelectMultiple(attrs={'size':'20'})}, 
     }
 
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    ordering = ['category__name', 'name', 'value']
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Product, ProductAdmin)
