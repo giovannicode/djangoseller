f4ee198a450e0e15923e9fd593f63e35827ab953
drone-xb81 <giovannicode7@gmail.com>
2015-04-07 14:05:57 -0500

    Add template order_detail.html

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 
 from products.models import Product
 from payments.models import Payment
