f28f2dfba1322f5fc930af0a24b06a5699491e92
drone-xb81 <giovannicode7@gmail.com>
2015-04-07 13:52:46 -0500

    Add OrderDetailView to urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,5 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.OrderListView.as_view(), name='index'),
+    url(r'^detail/(?P<pk>[0-9]+)$', views.OrderDetailView.as_view(), name='detail'),
 )
