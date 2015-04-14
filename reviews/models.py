84780375d18dbbcebb6ce2ed0a11c197b4298881
drone-xb81 <giovannicode7@gmail.com>
2015-04-14 14:21:57 -0500

    Create model Review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+class Review(models.Model):
+    product = models.ForeignKey(Product)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    RATING_CHOICES = (
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+    )
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comments = models.TextField()   
+
+    def __unicode__(self):
+        return self.product.name + ", " + str(self.user) + ", " + str(self.rating)
