b792cc0b3674cce819e91227d0f7cc541944a803
drone-xb81 <giovannicode7@gmail.com>
2015-04-22 13:02:53 -0500

    Add OrderOffice and OrderSerializer

--- office/views.py
+++ office/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, serializers
 
-# Create your views here.
+from orders.models import Order
+
+class OrderSerializer(serializers.OrderSerializer):
+    class Meta:
+        model = Order
+
+
+class OrderOffice(RetriveAPIView): 
+    model = Order
+    serializer_class = OrderSerializer
