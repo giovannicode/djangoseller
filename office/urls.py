afb5922de7c85812305ebf02fecf88b84209c03f
drone-xb81 <giovannicode7@gmail.com>
2015-04-23 11:32:42 -0500

    Add view 'AdminLogin'

--- office/urls.py
+++ office/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = patterns('',
-    url(r'^orders/list', views.OrderOffice.as_view(), name='order_list')
+    url(r'^orders/list', views.OrderListView.as_view(), name='order_list')
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
--- office/views.py
+++ office/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView 
+from django.contrib.admin import forms
 from rest_framework import generics, serializers
 
 from orders.models import Order
 
+class AdminLoginView(AnonymousRequiredMixin, FormView):
+    authenticated_redirect_url = u"/"
+    template_name = 'office/login.html'
+    form_class = AdminAuthenticationForm
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'office/object_list.html'
 
 
 class OrderOffice(generics.ListAPIView): 
